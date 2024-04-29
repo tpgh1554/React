4e4a21a6ae4d8f14f8b10ec1156598fbd8019388
Seho <tpgh1554@naver.com>
2024-04-29 12:38:34 +0900

    04.29 오전실습

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+import "./App.css";
+// JSX : JavaScript에 XML을 추가, 확장한 문법
+// html 태그 사용 시 반드시 닫힘 태그가 존재해야 함
+// 컴포넌트가 반환 될 때 태그는 한개여야 한다
+// 자바스크립트 코드를 중괄호를 사용해 데이터 바인딩 할 수 있음
+// 조건부 연산자 : {} 내에서 조건부 연산자 사용 가능
+// 조건부 랜더링 : && 연산자의 특징을 활용해서 조건에 따라 화면 렌더링
+// 인라인 스타일링 :
+
+function App() {
+  const name = "리액트";
+  const styleC = {
+    backgroundColor: "#222",
+    color: "royalblue",
+    fontSize: "2em",
+  };
+  return <p style={styleC}>{name === "리액트" && <h1>리액트입니다.</h1>}</p>;
+}
+
+export default App;
